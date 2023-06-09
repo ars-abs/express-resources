7ed9df0bbbd7ffd3355fa8f89e41c3db0dda3d26
ars-abs <ars@abstractit.in>
2023-06-09 18:37:37 +0530

    Introduce error and meta details.

--- src/responses/respond.js
+++ src/responses/respond.js
@@ -1,12 +1,14 @@
 import getStatus from './getStatus';
 
-const respond = ({ res, statusCode, message, data, results }) => {
+const respond = ({ res, statusCode, message, error, data, results, meta }) => {
 	res.status(statusCode);
 	res.json({
 		status: getStatus(statusCode),
 		message: message,
+		error: error,
 		results: results,
 		data: data,
+		meta: meta,
 	});
 };
 
