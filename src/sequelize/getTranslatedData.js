f6af84d4f52463f746fe0fb93bbe53ca2055d3f2
ars-abs <ars@abstractit.in>
2023-06-10 10:27:08 +0530

    Big fixed.

--- src/sequelize/getTranslatedData.js
+++ src/sequelize/getTranslatedData.js
@@ -1,7 +1,7 @@
 import { omit } from '@laufire/utils/collection';
 
 const getTranslatedData = (doc) => {
-	const data = doc.dataValues;
+	const data = doc ? doc.dataValues : {};
 	// eslint-disable-next-line no-underscore-dangle
 	const result = { ...data, id: data._id };
 
