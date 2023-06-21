66e4810611565f89fa224cc1c2f132bbdc869b4e
ars-abs <ars@abstractit.in>
2023-06-21 16:52:57 +0530

    Refactored.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import genResourceEndpoint from './resources/genResourceEndpoint';
 import buildEntities from './buildEntities';
 import syncEntities from './syncEntities';
-import { mapAsync, pipe } from './helpers';
+import { pipe } from './helpers';
 import buildCRUD from './buildCRUD';
 import buildContext from './buildContext';
 import buildValidators from './buildValidators';
 
-const expressResources = async (context) => {
-	const enrichContext = await pipe([
-		buildEntities,
-		syncEntities,
-		buildCRUD,
-		buildValidators,
-	], buildContext(context));
-
-	const { config: { resources }} = enrichContext;
-
-	await mapAsync(resources, (resource) =>
-		genResourceEndpoint({ ...enrichContext, data: { ...resource }}));
-};
+const expressResources = (context) => pipe([
+	buildEntities,
+	syncEntities,
+	buildCRUD,
+	buildValidators,
+	genResourceEndpoint,
+], buildContext(context));
 
 export { expressResources };
--- src/resources/genResourceEndpoint.js
+++ src/resources/genResourceEndpoint.js
@@ -1,20 +1,23 @@
 import operations from './operations';
 import enrichReq from './enrichReq';
+import { map } from '@laufire/utils/collection';
 
 const genResourceEndpoint = (context) => {
-	const { app, repoCRUD, validators, data: { name }} = context;
-	const repo = repoCRUD[name];
-	const validate = validators[name];
+	const { app, repoCRUD, validators, config: { resources }} = context;
 
-	app.use(enrichReq({ ...context, repo }));
+	map(resources, ({ name }) => {
+		const repo = repoCRUD[name];
+		const validate = validators[name];
 
-	/* eslint-disable function-paren-newline */
-	app.get(`/${ name }`, operations.getAll);
-	app.post(`/${ name }`, validate, operations.create);
-	app.get(`/${ name }/:id`, operations.get);
-	app.put(`/${ name }/:id`, validate, operations.update);
-	app.delete(`/${ name }/:id`, operations.remove);
-	/* eslint-enable function-paren-newline */
+		app.use(enrichReq({ ...context, repo }));
+		/* eslint-disable function-paren-newline */
+		app.get(`/${ name }`, operations.getAll);
+		app.post(`/${ name }`, validate, operations.create);
+		app.get(`/${ name }/:id`, operations.get);
+		app.put(`/${ name }/:id`, validate, operations.update);
+		app.delete(`/${ name }/:id`, operations.remove);
+		/* eslint-enable function-paren-newline */
+	});
 };
 
 export default genResourceEndpoint;
