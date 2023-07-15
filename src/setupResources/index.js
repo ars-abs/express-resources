1ca3040f5a2e86a77e9f89fbdbdab77d96c60075
ars-abs <ars@abstractit.in>
2023-07-15 13:43:31 +0530

    Moved buildCRUD from setupRoutes setupResources.

--- src/setupResources/index.js
+++ src/setupResources/index.js
@@ -2,12 +2,14 @@ import { pipe } from '../helpers';
 import buildEntities from './buildEntities';
 import makeRelations from './makeRelations';
 import syncEntities from './syncEntities';
+import buildCRUD from './buildCRUD';
 
 const setupResources = (context) => pipe([
 	buildEntities,
 	syncEntities,
 	makeRelations,
 	syncEntities,
+	buildCRUD,
 ], context);
 
 export default setupResources;
--- src/setupRoutes/index.js
+++ src/setupRoutes/index.js
@@ -1,10 +1,8 @@
 import { pipe } from '../helpers';
-import buildCRUD from './buildCRUD';
 import buildValidators from './buildValidators';
 import buildCRUDEndpoints from './buildCRUDEndpoints';
 
 const setupRoutes = (context) => pipe([
-	buildCRUD,
 	buildValidators,
 	buildCRUDEndpoints,
 ], context);
