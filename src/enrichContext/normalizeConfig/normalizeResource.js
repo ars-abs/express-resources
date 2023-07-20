be4f47817972e063c6a3085149310998405685fb
ars-abs <ars@abstractit.in>
2023-07-20 20:33:34 +0530

    Introduced extendPagination.

--- src/enrichContext/normalizeConfig/normalizeResource.js
+++ src/enrichContext/normalizeConfig/normalizeResource.js
@@ -1,4 +1,4 @@
-import { map, merge } from '@laufire/utils/collection';
+import { keys, map, merge } from '@laufire/utils/collection';
 import { convertSchema } from '../../lib/types';
 
 const translateSchema = ({ properties, ...rest }) => ({
@@ -7,15 +7,34 @@ const translateSchema = ({ properties, ...rest }) => ({
 		? { ...props, format: 'uuid' }
 		: props)),
 });
+const extendPagination = (pagination) => {
+	const { order, limit, offset } = pagination;
+	const { orders, ...rest } = order;
+	const querySchema = {
+		type: 'object',
+		properties: {
+			offset: offset,
+			limit: limit,
+			order: {
+				...rest,
+				type: 'string',
+				enum: keys(orders),
+			},
+		},
+	};
+
+	return { ...pagination, querySchema };
+};
 
 const normalizeResource = ({ resource, key, schemaExtensions }) => {
-	const { schema, name = key } = resource;
+	const { schema, name = key, pagination } = resource;
 	const extendedSchema = merge({ properties: schemaExtensions }, schema);
 	const translatedSchema = translateSchema(extendedSchema);
 
 	return {
 		...resource,
 		name: name,
+		pagination: extendPagination(pagination),
 		repoSchema: convertSchema({ ...translatedSchema, id: `${ name }` }).props,
 		schema: extendedSchema,
 	};
