00e14f3006c0eb411b9f56f15ae3d9ca5911c692
ars-abs <ars@abstractit.in>
2023-07-20 21:15:37 +0530

    Introduced getValidQuery.

--- src/setupRoutes/buildValidators/genValidateMiddleware.js
+++ src/setupRoutes/buildValidators/genValidateMiddleware.js
@@ -1,14 +1,17 @@
 import respond from '../../helpers/responses/respond';
 import ajv from './setupAjv';
+const getValidQuery = (query) => (query.offset && query.limit
+	? {
+		...query,
+		offset: Number(query.offset),
+		limit: Number(query.limit),
+	}
+	: query);
 
 const genValidateMiddleware = ({ schema, pagination: { querySchema }}) => (
 	{ body, query }, res, next
 ) => {
-	const validQuery = {
-		...query,
-		offset: Number(query.offset),
-		limit: Number(query.limit),
-	};
+	const validQuery = getValidQuery(query);
 
 	return ajv.validate(schema, body) && ajv.validate(querySchema, validQuery)
 		? next()
