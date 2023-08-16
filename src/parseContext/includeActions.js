df6c2755ee6ce0441625b9ba9156dc4b59e50a3d
dmdm-abs <dmdm@abstractit.in>
2023-08-16 10:59:36 +0530

    Introduced includeActions.

---
+++ src/parseContext/includeActions.js
@@ -0,0 +1,5 @@
+import actions from '../setupResources/actions';
+
+const includeActions = () => ({ actions });
+
+export default includeActions;
--- src/parseContext/index.js
+++ src/parseContext/index.js
@@ -2,11 +2,13 @@ import enrichResources from './enrichResources';
 import { pipe } from '../helpers';
 import extendSchema from './extendSchema';
 import denormalizeConfig from './denormalizeConfig';
+import includeActions from './includeActions';
 
 const parseContext = (context) => pipe([
 	extendSchema,
 	denormalizeConfig,
 	enrichResources,
+	includeActions,
 ], context);
 
 export default parseContext;
