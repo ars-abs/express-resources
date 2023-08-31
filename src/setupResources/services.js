7661f79db1b349362b6c2759b22dc83c0cf0f38b
dmdm-abs <dmdm@abstractit.in>
2023-08-31 11:58:29 +0530

    Refactored services.

--- src/setupResources/services.js
+++ src/setupResources/services.js
@@ -1,57 +1,10 @@
 import validate from './validate';
 
-const read = async (context) => {
-	const { store } = context;
-	const { error } = validate(context);
-	const response = error || await store(context);
-
-	return { error, ...response };
-};
-
-const list = async (context) => {
-	const { store } = context;
-	const { error } = validate(context);
-	const response = error || await store(context);
-
-	return { error, ...response };
-};
-
-const create = async (context) => {
-	const { store } = context;
-	const { error } = validate(context);
-	const response = error || await store(context);
-
-	return { error, ...response };
-};
-
-const update = async (context) => {
-	const { store } = context;
-	const { error } = validate(context);
-	const response = error || await store(context);
-
-	return { error, ...response };
-};
-
-const remove = async (context) => {
-	const { store } = context;
-	const { error } = validate(context);
-	const response = error || await store(context);
-
-	return { error, ...response };
-};
-
-const actions = {
-	read,
-	list,
-	create,
-	update,
-	remove,
-};
-
 const services = (context) => {
-	const { action } = context;
+	const { store } = context;
+	const { error } = validate(context);
 
-	return actions[action](context);
+	return error ? { error } : store(context);
 };
 
 export default services;
