27028292e9b57c2a0a43976ae7cfcc6e697494e0
dmdm-abs <dmdm@abstractit.in>
2023-09-06 12:27:08 +0530

    Removed includeLog.

--- src/includeLog.js
+++
@@ -1,5 +0,0 @@
-import log from 'log';
-
-const includeLog = () => ({ log });
-
-export default includeLog;
--- src/init.js
+++ src/init.js
@@ -4,12 +4,10 @@ import includeValidate from './includeValidate';
 import includeStore from './includeStore';
 import setupRoutes from './setupRoutes';
 import exportResources from './exportResources';
-import includeLog from './includeLog';
 
 const init = (context) => pipe([
 	denormalize,
 	includeValidate,
-	includeLog,
 	includeStore,
 	setupRoutes,
 	exportResources,
