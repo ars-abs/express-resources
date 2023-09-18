084d1552535ae2cf32d5fc492fce7d4630d955ab
ars-abs <ars@abstractit.in>
2023-09-18 20:02:11 +0530

    Removed setupRoutes.

--- src/init.js
+++ src/init.js
@@ -1,13 +1,11 @@
 import { pipe } from './helpers';
 import denormalize from './denormalize';
 import includeStore from './includeStore';
-import setupRoutes from './setupRoutes';
 import exportResources from './exportResources';
 
 const init = (context) => pipe([
 	denormalize,
 	includeStore,
-	setupRoutes,
 	exportResources,
 ], context);
 
