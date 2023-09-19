99b3310e9996d1cbf22d3f026f89993f473f70a5
ars-abs <ars@abstractit.in>
2023-09-19 12:44:34 +0530

    Removed unwanted function calls.

--- src/init.js
+++ src/init.js
@@ -1,10 +1,8 @@
 import { pipe } from './helpers';
-import denormalize from './denormalize';
 import includeStore from './includeStore';
 import exportResources from './exportResources';
 
 const init = (context) => pipe([
-	denormalize,
 	includeStore,
 	exportResources,
 ], context);
