4475990e7619df93025ce29eb0fc085619428dd6
ars-abs <ars@abstractit.in>
2023-11-03 15:31:27 +0530

    Introduce whereClause with configurable whitelists.

--- src/helpers/pagination.js
+++ src/helpers/pagination.js
@@ -1,19 +1,19 @@
-import { map } from '@laufire/utils/collection';
+import { map, select } from '@laufire/utils/collection';
 
 const getOptions = ({ config: { resources }, meta, name }) => {
-	const { pagination = {}} = resources[name];
+	const { pagination = {}, whiteList = [] } = resources[name];
 	const { offset: offsetVal, limit: limitVal, order: orderVal } = pagination;
-
 	const defaultValues = {
 		offset: offsetVal?.default,
 		limit: limitVal?.default,
 		order: orderVal?.default,
 	};
-	const { offset, limit, order } = { ...defaultValues, ...meta };
+	const { offset, limit, order, where } = { ...defaultValues, ...meta };
 
 	return {
 		offset: offset,
 		limit: limit,
+		where: where ? select(JSON.parse(where), whiteList) : {},
 		order: orderVal && map(orderVal.orders[order],
 			({ field, direction }) => [field, direction]),
 	};
